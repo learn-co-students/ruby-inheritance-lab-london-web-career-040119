2c22368cb3d5533040954feee6e5863e63da8c8e
Ay Has <ahashim983@yahoo.com>
2019-04-09 21:42:59 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative '../lib/user.rb'
+
+class Student < User
+
+  attr_accessor :knowledge
+
+  def initialize(knowledge = [])
+    @knowledge = knowledge
+  end
+
+  def learn(data)
+    @knowledge.push(data)
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
