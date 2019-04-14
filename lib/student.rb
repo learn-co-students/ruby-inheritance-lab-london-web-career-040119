bb0ed34723fbfa591ad3be24f3452189792d43f4
0118ayush <ayush_gehlot@hotmail.com>
2019-04-14 12:27:57 +0100

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(new_knowledge)
+    @knowledge.push(new_knowledge)
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
